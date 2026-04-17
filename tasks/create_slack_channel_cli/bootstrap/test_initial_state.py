b4cf3ba37ad586d30ec016ecdcff1a850e50acf2
Zhiming Ma <codes.icy@gmail.com>
2026-04-17 13:42:52 +0800

    data: remove invalid tasks.

--- tasks/create_slack_channel_cli/bootstrap/test_initial_state.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-import pytest
-
-def test_composio_cli_available():
-    assert shutil.which("composio") is not None, "composio CLI not found in PATH."
-
-def test_composio_api_key_set():
-    assert "COMPOSIO_API_KEY" in os.environ, "COMPOSIO_API_KEY environment variable is not set."
